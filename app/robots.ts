952f18eca1b8b1b787b36b7d4e84c2156a8d3d19
munthersj <munthersarraj@gmail.com>
2026-02-06 02:33:12 +0300

    searchConsole

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://sahm-delivery.ae/sitemap.xml",
+    sitemap: "https://batoto-delivery.cloud/sitemap.xml",
   };
 }
